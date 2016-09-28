5cf46238e2936ef53c6e5d6e52a4a0574914a02e
keik <k4t0.kei@gmail.com>
2016-09-28 23:42:52 +0900

    Implement isomorphic SSR

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: {
+    app: './lib/client/app'
+  },
+  output: {
+    path: './build/assets',
+    filename: '[name].js'
+  },
+  target: 'web',
+  module: {
+    loaders: [
+      {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel'}
+    ]
+  },
+  resolve: {extensions: ['', '.js', '.jsx']}
+
+}
