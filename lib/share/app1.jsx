da365301b6a589828d89cfa5352b3f6426f823e7
keik <k4t0.kei@gmail.com>
2016-10-03 08:49:27 +0900

    Add global styles

--- lib/share/app1.jsx
+++ lib/share/app1.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import globalStyles from './styles/global.css'
 import styles from './app1.css'
 
 export default class App1View extends React.Component {
   render () {
     return (
       <div className={styles.container}>
-        <h1>App1</h1>
-        <p className={styles.hello}>
-          Hello, {this.props.name}. This is App1 built from <code>lib/share/app1.jsx</code> + <code>lib/share/app1.css</code>.
-        </p>
+        <div className="container">
+          <h1>App1</h1>
+          <p className={styles.hello}>
+            Hello, {this.props.name}. This is App1 built from <code>lib/share/app1.jsx</code> + <code>lib/share/app1.css</code>.
+          </p>
+        </div>
       </div>
     )
   }
--- lib/share/app2.jsx
+++ lib/share/app2.jsx
@@ -5,10 +5,12 @@ export default class App2View extends React.Component {
   render () {
     return (
       <div className={styles.container}>
-        <h1>App2</h1>
-        <p className={styles.hello}>
-          Hello, {this.props.name}. This is App2 built from <code>lib/share/app2.jsx</code> + <code>lib/share/app2.css</code>.
-        </p>
+        <div className="container">
+          <h1>App2</h1>
+          <p className={styles.hello}>
+            Hello, {this.props.name}. This is App2 built from <code>lib/share/app2.jsx</code> + <code>lib/share/app2.css</code>.
+          </p>
+        </div>
       </div>
     )
   }
